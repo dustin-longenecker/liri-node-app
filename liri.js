0011512ed8c0025f11466643cdb5e462e89b3134
dustin-longenecker <dl-dev@outlook.com>
2018-08-26 19:51:07 -0600

    refractored code and added an object version of liri bot to liriObj.js. both are fully functional

--- liri.js
+++ liri.js
@@ -10,163 +10,134 @@ var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 
 //command line agument variables
-var command = process.argv[2];
-var argument = process.argv.slice(3).join("+");
+var cmd = process.argv[2];
+var arg = process.argv.slice(3).join("+");
+
+commandSwitch(cmd, arg);
+
 
 //main command switch statement function
-function commandSwitch(cmd, arg){
+function commandSwitch(cmd, arg) {
   switch (cmd) {
-      case 'concert-this':
-        concertThis(arg);
+    case 'concert-this':
+      concertThis(arg);
       break;
-      case 'spotify-this-song':
-        spotifyThis(arg);
+    case 'spotify-this-song':
+      spotifyThis(arg);
       break;
-      case 'movie-this':
-        movieThis(arg);
+    case 'movie-this':
+      movieThis(arg);
       break;
-      case 'do-what-it-says':
-        doWhatItSays();
+    case 'do-what-it-says':
+      doWhatItSays();
       break;
+    default:
+      console.log("Please use a valid command.")
+      return;
   }
 }
 // call main switch statement function
-commandSwitch(command, argument);
 //concert-this function
-function concertThis(arg){
+function concertThis(arg) {
   var artist = arg;
-  var queryUrl ="https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-  var concert = [];
-  request(queryUrl, function(error, response, body){
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&tracker_count=10";
+  request(queryUrl, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      // console.log("Title: " + JSON.parse(body).Title);
-      // console.log(JSON.parse(body));
-      // console.log(JSON.parse(body)[0].venue.name);
       body = JSON.parse(body);
-      for (var event in body){
-        console.log("Venue: " + body[event].venue.name)
-
-        appendFile("Venue: " + body[event].venue.name + "\n");
-        console.log("Location: " + body[event].
-        venue.city + ", " + body[event].venue.country);
-        appendFile("Location: " + body[event].
-        venue.city + ", " + body[event].venue.country + "\n");
-        // console.log("Date: " + body[event].datetime);
-        var m = moment(body[event].datetime).format('MM DD YYYY');
-        // , h:mm:ss a
-        // console.log(m);
-        m = m.split(" ").join("/");
-        // console.log(m);
-        // m[0] = m[0].split(" ").join("/");
-        // appendFile("DateTime:  " + m[0] + ", " + m[1] + "\n");
-        appendFile("Date: " + m + "\n");
-        console.log("Date: " + m);
+      for (var event in body) {
+        display("Venue: ", body[event].venue.name);
+        display("Location: ", body[event].venue.city + ", " + body[event].venue.region + ", " + body[event].venue.country);
+        var m = moment(body[event].datetime).format('MM/DD/YYYY, h:mm a').split(", ");
+        display("Date: ", m[0]);
+        display("Time: ", m[1]);
+        contentAdded();
       }
     }
-  })
+  });
 }
 //spotify-this function
-function spotifyThis(arg){
+function spotifyThis(arg) {
   var song = arg;
-  if(!song){
+  if (!song) {
     song = "The+Sign";
     console.log(song);
   }
-  spotify.search({ type: 'track', query: song }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-// console.log(data);
-console.log("Artist(s) Name: " + data.tracks.items[0].artists[0].name);
-console.log("Track Name: " + data.tracks.items[0].name);
-console.log("Preview URL: " + data.tracks.items[0].preview_url);
-console.log("Album: " + data.tracks.items[0].album.name);
-appendFile("Artist(s) Name: " + data.tracks.items[0].artists[0].name + "\n");
-appendFile("Track Name: " + data.tracks.items[0].name + "\n");
-appendFile("Preview URL: " + data.tracks.items[0].preview_url + "\n");
-appendFile("Album: " + data.tracks.items[0].album.name + "\n");
-
-});
+  spotify.search({
+    type: 'track',
+    query: song
+  }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    data = data.tracks.items[0];
+    // console.log(data);
+    display("Artist(s) Name: ", data.artists[0].name);
+    display("Track Name: ", data.name);
+    display("Preview URL: ", data.preview_url);
+    display("Album: ", data.album.name);
+    contentAdded();
+  });
 }
 // movie-this function
-function movieThis(arg){
+function movieThis(arg) {
   var movieName = arg;
-  if(!movieName){movieName="Mr.+Nobody"};
+  if (!movieName) {
+    movieName = "Mr.+Nobody"
+  };
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-  request(queryUrl, function(error, response, body){
+  request(queryUrl, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      console.log("Title: " + JSON.parse(body).Title);
-      appendFile("Title: " + JSON.parse(body).Title + "\n");
-
-      console.log("Year: " + JSON.parse(body).Year);
-      appendFile("Year: " + JSON.parse(body).Year + "\n");
-
-      console.log("imdbRating: " + JSON.parse(body).imdbRating);
-      appendFile("imdbRating: " + JSON.parse(body).imdbRating + "\n");
-
-      if(JSON.parse(body).Ratings[2]){
-      console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[2].Value);
-      appendFile("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[2].Value + "\n");
-
+      body = JSON.parse(body);
+      display("Title: ", body.Title);
+      display("Year: ", body.Year);
+      display("IMDB Rating: ", body.imdbRating);
+      if (body.Ratings[2]) {
+        display("Rotten Tomatoes Score: ", body.Ratings[2].Value);
       }
-      console.log("Country: " + JSON.parse(body).Country);
-      appendFile("Country: " + JSON.parse(body).Country + "\n");
-
-      console.log("Language: " + JSON.parse(body).Language);
-      appendFile("Language: " + JSON.parse(body).Language + "\n");
-
-      console.log("Plot: " + JSON.parse(body).Plot);
-      appendFile("Plot: " + JSON.parse(body).Plot + "\n");
-
-      console.log("Actors: " + JSON.parse(body).Actors);
-      appendFile("Actors: " + JSON.parse(body).Actors + "\n");
-
-
+      display("Country: ", body.Country);
+      display("Language: ", body.Language);
+      display("Plot: ", body.Plot);
+      display("Actors: ", body.Actors);
+      contentAdded();
     }
-  })
+  });
 }
+
 // do-what-it-says function
-function doWhatItSays(){
+function doWhatItSays() {
   fs.readFile("random.txt", "utf8", function(error, data) {
-    // If the code experiences any errors it will log the error to the console.
     if (error) {
       return console.log(error);
     }
-    // We will then print the contents of data
-    console.log(data);
-    // Then split it by commas (to make it more readable)
-    var dataArr = data.split(",");
-    var cmd = dataArr[0];
-    var arg = dataArr[1];
-    arg = arg.replace(/['"]+/g, '');
-    // We will then re-display the content as an array for later use.
-    console.log(dataArr);
-    arg = arg.split(' ').join("+");
-    console.log(arg);
-    commandSwitch(cmd, arg);
+    var dataArr = data.replace(/(\r\n|\n|\r)/gm, "").split(",");
+    for (var i = 0; i < dataArr.length; i += 2) {
+      var cmd = dataArr[i];
+      var arg = dataArr[i + 1].replace(/['"]+/g, '').split(' ').join("+");
+      commandSwitch(cmd, arg);
+    }
   });
-
 }
-
-
-
-// Next, we store the text file filename given to us from the command line.
+//create log.txt file
 var textFile = "log.txt";
-function appendFile(arg){
+// console.log / appendFile function
+function display(description, data) {
+  console.log(description + data);
+  appendFile(description + data + "\n");
+}
+function contentAdded(){
+  console.log("");
+  console.log("Content Added!");
+  console.log("-----------------------------------");
+  appendFile("-----------------------------------\n");
+}
+//appendFile function
+function appendFile(arg) {
   fs.appendFile(textFile, arg, function(err) {
-
-    // If an error was experienced we will log it.
     if (err) {
       console.log(err);
     }
-
-    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
     else {
-      console.log("Content Added!");
+      // console.log("Content Added!");
     }
-
   });
 }
-
-// Next, we append the contents "Hello Kitty" into the file.
-// If the file didn't exist, then it gets created on the fly.
---
+++ liriObj.js
@@ -0,0 +1,131 @@
+//requires
+require("dotenv").config();
+var request = require("request");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
+var moment = require("moment");
+
+//spotify constructor keys
+var spotify = new Spotify(keys.spotify);
+
+var liri_bot = {
+  cmd: process.argv[2],
+  arg: process.argv.slice(3).join("+"),
+  textFile: "log.txt",
+  switchCmd: function() {
+    switch (this.cmd) {
+      case 'concert-this':
+        this.concertThis(this.arg);
+        break;
+      case 'spotify-this-song':
+        this.spotifyThis(this.arg);
+        break;
+      case 'movie-this':
+        this.movieThis(this.arg);
+        break;
+      case 'do-what-it-says':
+        this.doWhatItSays();
+        break;
+      default:
+        console.log("Please use a valid command.")
+        return;
+      }
+    },
+    concertThis: function() {
+      var artist = this.arg;
+      var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&tracker_count=10";
+      request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          body = JSON.parse(body);
+          for (var event in body) {
+            liri_bot.display("Venue: ", body[event].venue.name);
+            liri_bot.display("Location: ", body[event].venue.city + ", " + body[event].venue.region + ", " + body[event].venue.country);
+            var m = moment(body[event].datetime).format('MM/DD/YYYY, h:mm a').split(", ");
+            liri_bot.display("Date: ", m[0]);
+            liri_bot.display("Time: ", m[1]);
+            liri_bot.contentAdded();
+          }
+        }
+      });
+    },
+    spotifyThis: function(){
+      var song = this.arg;
+      if (!song) {
+        song = "The+Sign";
+        console.log(song);
+      }
+      spotify.search({
+        type: 'track',
+        query: song
+      }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        data = data.tracks.items[0];
+        // console.log(data);
+        liri_bot.display("Artist(s) Name: ", data.artists[0].name);
+        liri_bot.display("Track Name: ", data.name);
+        liri_bot.display("Preview URL: ", data.preview_url);
+        liri_bot.display("Album: ", data.album.name);
+        liri_bot.contentAdded();
+      });
+    },
+    movieThis: function(){
+      var movieName = this.arg;
+      if (!movieName) {
+        movieName = "Mr.+Nobody"
+      };
+      var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+      request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          body = JSON.parse(body);
+          liri_bot.display("Title: ", body.Title);
+          liri_bot.display("Year: ", body.Year);
+          liri_bot.display("IMDB Rating: ", body.imdbRating);
+          if (body.Ratings[2]) {
+            liri_bot.display("Rotten Tomatoes Score: ", body.Ratings[2].Value);
+          }
+          liri_bot.display("Country: ", body.Country);
+          liri_bot.display("Language: ", body.Language);
+          liri_bot.display("Plot: ", body.Plot);
+          liri_bot.display("Actors: ", body.Actors);
+          liri_bot.contentAdded();
+        }
+      });
+    },
+    doWhatItSays: function(){
+      fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log(error);
+        }
+        var dataArr = data.replace(/(\r\n|\n|\r)/gm, "").split(",");
+        for (var i = 0; i < dataArr.length; i += 2) {
+          liri_bot.cmd = dataArr[i];
+          liri_bot.arg = dataArr[i + 1].replace(/['"]+/g, '').split(' ').join("+");
+          liri_bot.switchCmd();
+        }
+      });
+    },
+    display: function(description, data){
+      console.log(description + data);
+      this.appendFile(description + data + "\n");
+    },
+    appendFile: function() {
+      fs.appendFile(this.textFile, this.arg, function(err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // console.log("Content Added!");
+        }
+      });
+    },
+    contentAdded: function(){
+      console.log("");
+      console.log("Content Added!");
+      console.log("-----------------------------------");
+      this.appendFile("-----------------------------------\n");
+    }
+}
+liri_bot.switchCmd();
